0faf3e2e50966d820ce83b41a3b4c78d834192a0
陈小双 <chenxiaoshuang1@corp.netease.com>
2021-10-27 15:20:00 +0800

    编译调试：新建一个target: LDTest

---
+++ objc4-824/LDTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LDTest
+//
+//  Created by littledou on 2021/10/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
